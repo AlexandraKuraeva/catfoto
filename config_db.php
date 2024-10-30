55a4a8b09ea6973ebcf027e5144722ac3dfd750d
Alexandra <kuraeva@lz.media>
2024-10-30 15:53:06 +0400

    create data

--- config_db.php
+++ config_db.php
@@ -3,8 +3,8 @@
 
 define("DB_HOST", "localhost");
 
-define("DB_USER", "alexakjm_catfoto");
+define("DB_USER", "roor");
 
-define("DB_PASSWORD", "5kvCSIw&AbI3");
+define("DB_PASSWORD", "");
 
-define("DB_NAME", "alexakjm_catfoto");
+define("DB_NAME", "catfoto");
